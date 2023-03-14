71cd4b1997e65c375a58c810753c0a99f5d090ec
Rafaela <104620799+rafaeladev@users.noreply.github.com>
2023-03-14 16:44:11 +0100

    board game created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Die from "./Die.js";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    function allNewDice() {
+        const newDice = [];
+        for (let i = 0; i < 10; i++) {
+            newDice.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+            });
+        }
+        return newDice;
+    }
+    //console.log(allNewDice());
+
+    const [dice, setDice] = React.useState(allNewDice());
+
+    // console.log(dice);
+    // console.log(setDice);
+
+    const diceElements = dice.map((die) => <Die number={die.value} />);
+
+    function rollDice() {
+        setDice(() => allNewDice());
+    }
+
+    return (
+        <main>
+            <div className="dice-container">{diceElements}</div>
+            <button className="roll-dice" onClick={rollDice}>
+                Roll
+            </button>
+        </main>
+    );
 }
 
 export default App;
