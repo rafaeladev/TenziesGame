cfd4cd25d19c228698216b631136217ece881c09
Rafaela <104620799+rafaeladev@users.noreply.github.com>
2023-03-14 18:02:42 +0100

    final of the game

--- src/App.js
+++ src/App.js
@@ -1,32 +1,83 @@
 import Die from "./Die.js";
 import React from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+    const [dice, setDice] = React.useState(allNewDice());
+    const [tenzies, setTenzies] = React.useState(false);
+
+    React.useEffect(() => {
+        const allHeld = dice.every((die) => die.isHeld);
+        const firstValue = dice[0].value;
+        const allSameValue = dice.every((die) => die.value === firstValue);
+        if (allHeld && allSameValue) {
+            setTenzies(true);
+            console.log("You won!");
+        }
+    }, [dice]);
+
+    // cerate the board of dices
     function allNewDice() {
         const newDice = [];
         for (let i = 0; i < 10; i++) {
-            newDice.push({
-                value: Math.ceil(Math.random() * 6),
-                isHeld: false,
-            });
+            newDice.push(generateNewDie());
         }
         return newDice;
     }
+
+    //Generate a new die
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid(),
+        };
+    }
+
     //console.log(allNewDice());
-
-    const [dice, setDice] = React.useState(allNewDice());
-
-    // console.log(dice);
+    //console.log(dice);
     // console.log(setDice);
 
-    const diceElements = dice.map((die) => <Die number={die.value} />);
+    // keep the Dice that was clicked with the color green
+    function holdDice(id) {
+        setDice((prevDice) => {
+            return prevDice.map((die) => {
+                return die.id === id
+                    ? {
+                          ...die,
+                          isHeld: !die.isHeld,
+                      }
+                    : die;
+            });
+        });
+    }
 
+    // affiche tous les elements de l'array Dice sur le dom
+    const diceElements = dice.map((die) => (
+        <Die
+            key={die.id}
+            number={die.value}
+            isHeld={die.isHeld}
+            holdDice={() => holdDice(die.id)}
+        />
+    ));
+
+    // change les chiffres des dices qui ne sont pas selectionés
     function rollDice() {
-        setDice(() => allNewDice());
+        setDice((oldDice) =>
+            oldDice.map((die) => {
+                return die.isHeld ? die : generateNewDie();
+            })
+        );
     }
 
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">
+                Roll until all dice are the same. Click each die to freeze it at
+                its current value between rolls.
+            </p>
             <div className="dice-container">{diceElements}</div>
             <button className="roll-dice" onClick={rollDice}>
                 Roll
--- src/Die.js
+++ src/Die.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Die = (props) => {
+export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white",
+    };
     return (
-        <div className="die-face">
+        <div className="die-face" style={styles} onClick={props.holdDice}>
             <h2 className="die-num">{props.number}</h2>
         </div>
     );
-};
-
-export default Die;
+}
